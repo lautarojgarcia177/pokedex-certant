fc4653eb3a0486f5a009830951b813d1a6d0204e
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-07-18 16:12:28 -0300

    added pokemon columns

--- src/main/java/com/certant/pokedex/entities/Pokemon.java
+++ src/main/java/com/certant/pokedex/entities/Pokemon.java
@@ -1,18 +1,32 @@
 package com.certant.pokedex.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Pokemon {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "POKEMON_ID")
     private long id;
 
+    @Column(name = "NAME")
     private String name;
+
+    @Column(name = "IMG")
+    private String img;
+
+    @Column(name = "FOUND_ON_LEVEL")
+    private short foundOnLevel;
+
+    @Column(name = "EVOLUTION_ON_LEVEL")
+    private short evolutionOnLevel;
+
+    @ManyToMany(targetEntity=PokemonAbility.class, cascade = {CascadeType.ALL})
+    @JoinTable(name="POKEMONS_ABILITIES", joinColumns = { @JoinColumn(name="POKEMON_ID") }, inverseJoinColumns = { @JoinColumn(name="ABILITY_ID")})
+    private Set<PokemonAbility> abilities;
 
     public String getName() {
         return name;
@@ -21,5 +35,30 @@ public class Pokemon {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public short getFoundOnLevel() {
+        return foundOnLevel;
+    }
+
+    public void setFoundOnLevel(short foundOnLevel) {
+        this.foundOnLevel = foundOnLevel;
+    }
+
+    public short getEvolutionOnLevel() {
+        return evolutionOnLevel;
+    }
+
+    public void setEvolutionOnLevel(short evolutionOnLevel) {
+        this.evolutionOnLevel = evolutionOnLevel;
+    }
+
 
 }
---
+++ src/main/java/com/certant/pokedex/entities/PokemonAbility.java
@@ -0,0 +1,34 @@
+package com.certant.pokedex.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class PokemonAbility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ABILITY_ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
